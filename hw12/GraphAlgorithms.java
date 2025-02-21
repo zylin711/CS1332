0a0fc7a26f60161b0199977053a1759d03257080
Yilin Zheng <zylin0711@gmail.com>
2025-02-21 00:00:27 -0500

    done with hw12

--- hw12/GraphAlgorithms.java
+++ hw12/GraphAlgorithms.java
@@ -1,4 +1,112 @@
-package PACKAGE_NAME;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
+/**
+ * Your implementation of various graph traversal algorithms.
+ */
 public class GraphAlgorithms {
+
+    /**
+     * Performs a breadth first search (bfs) on the input graph, starting at
+     * the parameterized starting vertex.
+     *
+     * When exploring a vertex, explore in the order of neighbors returned by
+     * the adjacency list. Failure to do so may cause you to lose points.
+     *
+     * You may import/use java.util.Set, java.util.List, java.util.Queue, and
+     * any classes that implement the aforementioned interfaces, as long as they
+     * are efficient.
+     *
+     * The only instance of java.util.Map that you should use is the adjacency
+     * list from graph. DO NOT create new instances of Map for BFS
+     * (storing the adjacency list in a variable is fine).
+     *
+     * DO NOT modify the structure of the graph. The graph should be unmodified
+     * after this method terminates.
+     *
+     * You may assume that the passed in start vertex and graph will not be null.
+     * You may assume that the start vertex exists in the graph.
+     *
+     * @param <T>   The generic typing of the data.
+     * @param start The vertex to begin the bfs on.
+     * @param graph The graph to search through.
+     * @return List of vertices in visited order.
+     */
+    public static <T> List<Vertex<T>> bfs(Vertex<T> start, Graph<T> graph) {
+        List<Vertex<T>> visited = new ArrayList<>();
+        Queue<Vertex<T>> queue = new LinkedList<>();
+        Set<Vertex<T>> seen = new HashSet<>();
+
+        queue.add(start);
+        seen.add(start);
+
+        while (!queue.isEmpty()) {
+            Vertex<T> current = queue.poll();
+            visited.add(current);
+
+            for (VertexDistance<T> neighbor : graph.getAdjList().get(current)) {
+                Vertex<T> vertex = neighbor.getVertex();
+                if (!seen.contains(vertex)) {
+                    queue.add(vertex);
+                    seen.add(vertex);
+                }
+            }
+        }
+
+        return visited;
+    }
+
+    /**
+     * Performs a depth first search (dfs) on the input graph, starting at
+     * the parameterized starting vertex.
+     *
+     * When exploring a vertex, explore in the order of neighbors returned by
+     * the adjacency list. Failure to do so may cause you to lose points.
+     *
+     * NOTE: This method should be implemented recursively. You may need to
+     * create a helper method.
+     *
+     * You may import/use java.util.Set, java.util.List, and any classes that
+     * implement the aforementioned interfaces, as long as they are efficient.
+     *
+     * The only instance of java.util.Map that you may use is the adjacency list
+     * from graph. DO NOT create new instances of Map for DFS
+     * (storing the adjacency list in a variable is fine).
+     *
+     * DO NOT modify the structure of the graph. The graph should be unmodified
+     * after this method terminates.
+     *
+     * You may assume that the passed in start vertex and graph will not be null.
+     * You may assume that the start vertex exists in the graph.
+     *
+     * @param <T>   The generic typing of the data.
+     * @param start The vertex to begin the dfs on.
+     * @param graph The graph to search through.
+     * @return List of vertices in visited order.
+     */
+    public static <T> List<Vertex<T>> dfs(Vertex<T> start, Graph<T> graph) {
+        List<Vertex<T>> visited = new ArrayList<>();
+        Set<Vertex<T>> seen = new HashSet<>();
+        dfsHelper(start, graph, visited, seen);
+        return visited;
+    }
+
+    private static <T> void dfsHelper(Vertex<T> current, Graph<T> graph, List<Vertex<T>> visited, Set<Vertex<T>> seen) {
+        visited.add(current);
+        seen.add(current);
+
+        for (VertexDistance<T> neighbor : graph.getAdjList().get(current)) {
+            Vertex<T> vertex = neighbor.getVertex();
+            if (!seen.contains(vertex)) {
+                dfsHelper(vertex, graph, visited, seen);
+            }
+        }
+    }
+
 }
--- hw12/GraphAlgorithmsTest.java
+++ hw12/GraphAlgorithmsTest.java
@@ -1,4 +1,76 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class GraphAlgorithmsTest {
+
+    public static void main(String[] args) {
+        testBfs();
+        testDfs();
+    }
+
+    /**
+     * Test the BFS method with a simple graph.
+     */
+    private static void testBfs() {
+        System.out.println("Testing BFS...");
+
+        // Create vertices
+        Vertex<String> a = new Vertex<>("A");
+        Vertex<String> b = new Vertex<>("B");
+        Vertex<String> c = new Vertex<>("C");
+        Vertex<String> d = new Vertex<>("D");
+        Vertex<String> e = new Vertex<>("E");
+
+        // Create edges
+        Set<Edge<String>> edges = new HashSet<>(Arrays.asList(
+                new Edge<>(a, b, 1),
+                new Edge<>(a, c, 1),
+                new Edge<>(b, d, 1),
+                new Edge<>(c, d, 1),
+                new Edge<>(d, e, 1)
+        ));
+
+        // Create graph
+        Set<Vertex<String>> vertices = new HashSet<>(Arrays.asList(a, b, c, d, e));
+        Graph<String> graph = new Graph<>(vertices, edges);
+
+        // Perform BFS
+        List<Vertex<String>> bfsResult = GraphAlgorithms.bfs(a, graph);
+        System.out.println("BFS result: " + bfsResult);
+        // Expected Output [A, B, C, D, E]
+    }
+
+    /**
+     * Test the DFS method with a simple graph.
+     */
+    private static void testDfs() {
+        System.out.println("Testing DFS...");
+
+        // Create vertices
+        Vertex<String> a = new Vertex<>("A");
+        Vertex<String> b = new Vertex<>("B");
+        Vertex<String> c = new Vertex<>("C");
+        Vertex<String> d = new Vertex<>("D");
+        Vertex<String> e = new Vertex<>("E");
+
+        // Create edges
+        Set<Edge<String>> edges = new HashSet<>(Arrays.asList(
+                new Edge<>(a, b, 1),
+                new Edge<>(a, c, 1),
+                new Edge<>(b, d, 1),
+                new Edge<>(c, d, 1),
+                new Edge<>(d, e, 1)
+        ));
+
+        // Create graph
+        Set<Vertex<String>> vertices = new HashSet<>(Arrays.asList(a, b, c, d, e));
+        Graph<String> graph = new Graph<>(vertices, edges);
+
+        // Perform DFS
+        List<Vertex<String>> dfsResult = GraphAlgorithms.dfs(a, graph);
+        System.out.println("DFS result: " + dfsResult);
+        // Expected Output [A, B, D, E, C]
+    }
 }
